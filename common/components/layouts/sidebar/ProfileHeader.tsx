a612da291b360f9b7e8d4e262b1a6918327bf904
Trungduong2111 <133733002+Trungduong2111@users.noreply.github.com>
2025-11-21 05:07:45 +0000

    Update name

--- common/components/layouts/sidebar/ProfileHeader.tsx
+++ common/components/layouts/sidebar/ProfileHeader.tsx
@@ -32,7 +32,7 @@ const ProfileHeader = ({ expandMenu, imageSize }: ProfileHeaderProps) => {
 
       <div className="mt-1 flex items-center gap-2 lg:mt-4">
         <Link href="/" passHref>
-          <h2 className="flex-grow text-lg font-medium lg:text-xl">Nguyễn Trùng Dương</h2>
+          <h2 className="flex-grow text-lg font-medium lg:text-xl">Trùng Dương</h2>
         </Link>
 
         <Tooltip title="Verified">
